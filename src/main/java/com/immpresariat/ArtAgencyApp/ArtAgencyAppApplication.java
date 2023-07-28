2e12d5dc3d3d3239c72e7bb2105e8492db546892
Michał Mościcki <michal.moscicki.cb@gmail.com>
2023-07-28 11:35:44 +0200

    Initial comit

---
+++ src/main/java/com/immpresariat/ArtAgencyApp/ArtAgencyAppApplication.java
@@ -0,0 +1,13 @@
+package com.immpresariat.ArtAgencyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtAgencyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtAgencyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immpresariat/ArtAgencyApp/ArtAgencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immpresariat.ArtAgencyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtAgencyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
