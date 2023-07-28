95dcb26122c17df437defa60579f3539bc1be3c0
Michał Mościcki <michal.moscicki.cb@gmail.com>
2023-07-28 15:55:52 +0200

    feat: add Institution repository

--- src/main/java/com/immpresariat/ArtAgencyApp/models/Institution.java
+++ src/main/java/com/immpresariat/ArtAgencyApp/models/Institution.java
@@ -1,2 +1,26 @@
-package com.immpresariat.ArtAgencyApp.models;public class Institution {
+package com.immpresariat.ArtAgencyApp.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Institution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Name must not be blank")
+    private String name;
+    @NotBlank(message = "City must not be blank")
+    private String city;
+    private boolean alreadyCooperated = false;
+    @Size(max = 500, message = "Notes can not be longer then 500 characters")
+    private String notes;
 }
---
+++ src/main/java/com/immpresariat/ArtAgencyApp/repository/InstitutionRepository.java
@@ -0,0 +1,8 @@
+package com.immpresariat.ArtAgencyApp.repository;
+
+import com.immpresariat.ArtAgencyApp.models.Institution;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+
+}
